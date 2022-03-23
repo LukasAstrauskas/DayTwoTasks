d5aba66fee6287a512f2e5182cdf239ac72daa8d
LukasA <lukas.astrauskas@gmail.com>
2022-03-23 10:03:26 +0200

    test

---
+++ src/com/BootCamp/DayThreeTasks.java
@@ -0,0 +1,13 @@
+package com.BootCamp;
+
+import java.lang.reflect.Array;
+
+public class DayThreeTasks {
+
+    public void taskOne(){
+        int[][] table = new int[10][10];
+
+
+    }
+
+}
--- src/com/BootCamp/Main.java
+++ src/com/BootCamp/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        DayTwoTasks dTT = new DayTwoTasks();
-        boolean a = dTT.taskSix(6,5);
-        boolean b = dTT.taskSix(0,5);
-        boolean c = dTT.taskSix(1000,1500);
-        System.out.println(a+"  "+b+"  "+c);
+
 
 
     }
